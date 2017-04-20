74692b3d9f8f9617efbb5ff7998702c6ca76071e
Štěpán Škorpil <stopka@skorpils.cz>
2017-04-20 14:21:15 +0200

    Extracted panel and tab templates out of Class file Replaced icon with a svg icon

--- src/GitVersionPanel.php
+++ src/GitVersionPanel.php
@@ -19,8 +19,10 @@ class GitVersionPanel implements IBarPanel
 
 	public function getPanel()
 	{
-		$this->parseHead();
-		return '<h1>Git Version</h1><p>Revision: ' . $this->getCurrentCommitHash() . '</p>';
+        $this->parseHead();
+        ob_start(function () {});
+        require __DIR__ . '/templates/MaintenancePanel.panel.phtml';
+        return ob_get_clean();
 	}
 
 	protected function getCurrentBranchName()
@@ -45,8 +47,9 @@ class GitVersionPanel implements IBarPanel
 
 	public function getTab()
 	{
-		return '<img alt="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAedJREFUeNqMk79LXEEQx+e8FyKIGAKBNCKxiXiIP4r8EB9iEUFFSKFgDPkDrjpLi3QhfYSApSAoop34I4WNXAoVq9icheH8dXLxok9Ofd6+3Vtnxn2PxzuiGfgwszO735vZtxf7Mv8BIlZn/AU8YJ+HZ6FKKQUhPsVjlkNQHKlVQGYpKX3BAStuTb9rHeLFyvbMtFTyDMPl+7qokp4EpKus9FJPy3u4EXmGYspRzeypgAU8z4tLqdJ2oh/KugjO1R+GYspRjfYgEIVH8ISgYezVjbkk+lG77Q0X1rbmyc0ik4j61wiWKAnyPw2jN8Llgsl/DO19iSSN4G5wB9gBhHHFNRPNI8nOlt4Uzp7B+BXlWIB+KYxbchmznkJ+IxpJPXqsobtjEMXkJq7fkkBsZDwRHkm3tzdzcLzvQEfChvrnjVBX+5Rz679m4NmTBtCiGn6kF0DrsmX5rfhG7ZP19QzDUSED23tL2FExqBddB2JeDY+FAsElBuZfYnpnAXKHebg4u6RPybmm1hdQdK4gd3CKh7W9+D2rKjooGYFctkCHJzD8ipwi3/7mz1Mnh3z4Na637t6BeRCBgFdiX8ifkxsLlSb3944h+hkt/0n6JuRdR9G8OTRW8ZBCfyZWz2xmk34M/2G3AgwAYPB4kNQnLB4AAAAASUVORK5CYII=" />'
-		. $this->getCurrentBranchName();
+        ob_start(function () {});
+        require __DIR__ . '/templates/MaintenancePanel.tab.phtml';
+        return ob_get_clean();
 	}
 
 	private function parseHead()
