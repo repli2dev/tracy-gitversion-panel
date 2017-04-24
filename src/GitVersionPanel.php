3617ba9b663b751c492cea1879ccbe01fdfa0b73
Štěpán Škorpil <stepan.s@gmail.com>
2017-04-24 09:18:46 +0200

    Better look, detailed info (#2)
    Refactored for more details
    
    - Extracted templates
    - New layout
    - Support for named branches (feature/name, hotfix/name...)
    - New SVG icon

--- src/GitVersionPanel.php
+++ src/GitVersionPanel.php
@@ -8,11 +8,14 @@ use Tracy\IBarPanel;
  *
  * @author Jan Drábek
  * @author Vojtěch Vondra
+ * @author Štěpán Škorpil
  */
 class GitVersionPanel implements IBarPanel
 {
 
 	private $read = false;
+
+	private $dir;
 
 	private $branch;
 	private $commit;
@@ -20,7 +23,39 @@ class GitVersionPanel implements IBarPanel
 	public function getPanel()
 	{
 		$this->parseHead();
-		return '<h1>Git Version</h1><p>Revision: ' . $this->getCurrentCommitHash() . '</p>';
+		ob_start(function () {
+		});
+		require __DIR__ . '/templates/GitVersionPanel.panel.phtml';
+		return ob_get_clean();
+	}
+
+	protected function getLogTail($rowCount = 5)
+	{
+		$dir = $this->findGitDir();
+		$logHead = $dir . '/logs/HEAD';
+		if (!$dir || !is_readable($logHead)) {
+			return [];
+		}
+		$fp = fopen($logHead, 'r');
+		fseek($fp, -1, SEEK_END);
+		$pos = ftell($fp);
+		$log = "";
+		$rowCounter = -1;
+		while ($rowCounter <= $rowCount && $pos >= 0) {
+			$char = fgetc($fp);
+			$log = $char . $log;
+			if ($char == "\n")
+				$rowCounter++;
+			fseek($fp, $pos--);
+		}
+		$result = [];
+		foreach (explode("\n", trim($log)) as $row) {
+			$input = [];
+			list($row, $input['action']) = explode("\t", $row, 2);
+			list($input['from'], $input['to'], $input['user']) = explode(" ", $row, 3);
+			$result[] = $input;
+		}
+		return $result;
 	}
 
 	protected function getCurrentBranchName()
@@ -45,35 +80,50 @@ class GitVersionPanel implements IBarPanel
 
 	public function getTab()
 	{
-		return '<img alt="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAedJREFUeNqMk79LXEEQx+e8FyKIGAKBNCKxiXiIP4r8EB9iEUFFSKFgDPkDrjpLi3QhfYSApSAoop34I4WNXAoVq9icheH8dXLxok9Ofd6+3Vtnxn2PxzuiGfgwszO735vZtxf7Mv8BIlZn/AU8YJ+HZ6FKKQUhPsVjlkNQHKlVQGYpKX3BAStuTb9rHeLFyvbMtFTyDMPl+7qokp4EpKus9FJPy3u4EXmGYspRzeypgAU8z4tLqdJ2oh/KugjO1R+GYspRjfYgEIVH8ISgYezVjbkk+lG77Q0X1rbmyc0ik4j61wiWKAnyPw2jN8Llgsl/DO19iSSN4G5wB9gBhHHFNRPNI8nOlt4Uzp7B+BXlWIB+KYxbchmznkJ+IxpJPXqsobtjEMXkJq7fkkBsZDwRHkm3tzdzcLzvQEfChvrnjVBX+5Rz679m4NmTBtCiGn6kF0DrsmX5rfhG7ZP19QzDUSED23tL2FExqBddB2JeDY+FAsElBuZfYnpnAXKHebg4u6RPybmm1hdQdK4gd3CKh7W9+D2rKjooGYFctkCHJzD8ipwi3/7mz1Mnh3z4Na637t6BeRCBgFdiX8ifkxsLlSb3944h+hkt/0n6JuRdR9G8OTRW8ZBCfyZWz2xmk34M/2G3AgwAYPB4kNQnLB4AAAAASUVORK5CYII=" />'
-		. $this->getCurrentBranchName();
+		ob_start(function () {
+		});
+		require __DIR__ . '/templates/GitVersionPanel.tab.phtml';
+		return ob_get_clean();
+	}
+
+	private function findGitDir()
+	{
+		if ($this->dir)
+			return $this->dir;
+
+		$scriptPath = $_SERVER['SCRIPT_FILENAME'];
+		$dir = realpath(dirname($scriptPath));
+		while ($dir !== false) {
+			flush();
+			$currentDir = $dir;
+			$dir .= '/..';
+			$dir = realpath($dir);
+			$gitDir = $dir . '/.git';
+			if (is_dir($gitDir)) {
+				$this->dir = $gitDir;
+				return $gitDir;
+			}
+			// Stop recursion to parent on root directory
+			if ($dir == $currentDir) {
+				break;
+			}
+		}
+		return NULL;
 	}
 
 	private function parseHead()
 	{
 		if (!$this->read) {
-			$scriptPath = $_SERVER['SCRIPT_FILENAME'];
-			$dir = realpath(dirname($scriptPath));
-			while ($dir !== false && !is_dir($dir . '/.git')) {
-				flush();
-				$currentDir = $dir;
-				$dir .= '/..';
-				$dir = realpath($dir);
+			$dir = $this->findGitDir();
 
-				// Stop recursion to parent on root directory
-				if ($dir == $currentDir) {
-					break;
-				}
-			}
-
-			$head = $dir . '/.git/HEAD';
+			$head = $dir . '/HEAD';
 			if ($dir && is_readable($head)) {
 				$branch = file_get_contents($head);
 				if (strpos($branch, 'ref:') === 0) {
-					$parts = explode('/', $branch);
+					$parts = explode('/', $branch, 3);
 					$this->branch = $parts[2];
 
-					$commitFile = $dir . '/.git/' . trim(substr($branch, 5, strlen($branch)));
+					$commitFile = $dir . '/' . trim(substr($branch, 5, strlen($branch)));
 					if (is_readable($commitFile)) {
 						$this->commit = file_get_contents($commitFile);
 					}
